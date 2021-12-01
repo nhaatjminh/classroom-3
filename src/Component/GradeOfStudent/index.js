6b9fe3b40fcf5373ab4fe68ff6b5f6e31d63c23a
nhaatjminh <minhphan.240199@gmail.com>
2021-12-01 14:03:27 +0700

    fix update grade v2

--- src/Component/GradeOfStudent/index.js
+++ src/Component/GradeOfStudent/index.js
@@ -12,32 +12,31 @@ const GradeOfStudent = ({dataGrade}) => {
 
     const onBlurHandler = (e) => {
         e.preventDefault();
-        console.log(dataGrade.student_id + ": " + grade)
+        console.log(dataGrade.student_id + ": " + grade);
 
-        // console.log("huhu");
-        // var myHeaders = new Headers();
-        // myHeaders.append("Authorization", "Bearer " + localStorage.getItem("token"));
-        // myHeaders.append("Content-Type", "application/json");
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer " + localStorage.getItem("token"));
+        myHeaders.append("Content-Type", "application/json");
 
 
-        // var raw = JSON.stringify({
-        //     "student_id": dataGrade.student_id,
-        //     "grade": grade
-        // });
+        var raw = JSON.stringify({
+            "student_id": 15,
+            "grade": grade
+        });
 
-        // var requestOptions = {
-        //     method: 'POST',
-        //     headers: myHeaders,
-        //     body: raw,
-        //     redirect: 'follow'
-        // };
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
 
-        // fetch(process.env.REACT_APP_API_UR + "grades/update/" + params.id + "/" + dataGrade.assignment_id, requestOptions)
-        // .then(response => response.json())
-        // .then(result => {
-        //     console.log("update grade successful");
-        // })
-        // .catch(error => console.log('error', error));
+        fetch(process.env.REACT_APP_API_URL + "grades/update/" + params.id + "/" + dataGrade.assignment_id, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            console.log("update grade successful");
+        })
+        .catch(error => console.log('error', error));
     }
     
     return(
